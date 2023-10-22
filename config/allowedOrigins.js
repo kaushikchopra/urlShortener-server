cbe1be964b156f66eb54a9f1f57072e207e5ee32
S Kaushik Chopra <kaushikchopra2908@gmail.com>
2023-10-22 21:34:27 +0530

    Update CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:8070',
-    'http://localhost:8070',
-    'http://localhost:3000'
-]; // Change this after deployment (i.e) only the website domain and react domain should be set
+    'https://urlsht.netlify.app',
+    'https://short-url-prbk.onrender.com'
+];
 
 export default allowedOrigins;
--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const clientURL = "http://localhost:3000"
-const BASE_URL = "http://localhost:8070"; // For Short URL
+const BASE_URL = "https://short-url-prbk.onrender.com"; // For Short URL
 const DAILY_LIMIT = 10;
 const MONTHLY_LIMIT = 100;
 
